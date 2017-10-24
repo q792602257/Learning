804c5309851bce08c4234ae630abad184b7eb98b
JerryYan0912 <yan1hao@outlook.com>
2017-10-24 12:15:28 +0800

    ImagePrint Api Some Function Modified

--- Python/ImagePrint.py
+++ Python/ImagePrint.py
@@ -4,25 +4,43 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import socket
+import sqlite3 as sq
+import _winreg as reg
+import glob
 import json
 
 class ImagePrint():
 	path=False
 	JobID=False
+	def __init__(self):
+		try:
+			key = reg.OpenKey(reg.HKEY_LOCAL_MACHINE,'SOFTWARE\\Code Industry\\ImagePrinterPro\\')
+			self.JobID,t=reg.QueryValueEx(key,'jobid')
+			self.Pre,t=reg.QueryValueEx(key,"image_name")
+			self.Path,t=reg.QueryValueEx(key,"path")
+			print self.JobID,self.Pre,self.Path
+		except Exception as e:
+			sys.exit()
 	def getJobID(self):
 		try:
-			f=open(os.path.join(self.path,'JobID'),'r+')
-			self.JobID=f.readline()
-			f.close()
+			key = reg.OpenKey(reg.HKEY_LOCAL_MACHINE,'SOFTWARE\\Code Industry\\ImagePrinterPro\\')
+			self.JobID,t=reg.QueryValueEx(key,'jobid')
 		except Exception as e:
 			sys.exit()
 		return self.JobID
-	def genJobID(self):
+	def getPriview(self,filename):
 		pass
+class FilePrint():
+	printerName="Unkown"
 	def printFile(self,filename):
 		pass
+# "C:\PROGRAM FILES\FOXIT SOFTWARE\FOXIT READER\FOXITREADER.EXE" /q /t C:\Users\yan1h\Documents\1.pdf "Send To OneNote 2016"
+# "C:\Program Files\Microsoft Office\Root\Office16\EXCEL.EXE" /q C:\Users\yan1h\Documents\1.xlsx /j "Send To OneNote 2016"
+# "C:\Program Files\Microsoft Office\Root\Office16\WINWORD.EXE" /j "%1" "%2"
+# %SystemRoot%\system32\NOTEPAD.EXE /pt %1
+# "%SystemRoot%\System32\rundll32.exe" "%SystemRoot%\System32\shimgvw.dll",ImageView_PrintTo /pt C:\Users\yan1h\Documents\Learning\wallpaper\19484.jpg "Send To OneNote 2016"
 	
-class Socket(ImagePrint):
+class Socket():
 	addr='0.0.0.0'
 	port=9999
 	s=False
@@ -47,3 +65,4 @@ class Socket(ImagePrint):
 			self.con.close()
 	def Send(self,data):
 		pass
+print ImagePrint().getJobID()
