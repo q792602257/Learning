178361ed3ae71213387425a5f84e0296ce029411
q792602257 <792602257@qq.com>
2017-11-05 20:28:59 +0800

    Add Some DNN Samples

--- Python/Keras/0.py
+++ Python/Keras/0.py
@@ -18,14 +18,13 @@ def cnn():
     model = Sequential()
 
     model.add(Convolution2D(filters=56, kernel_size=(7,7),
-                            padding='same', input_shape=(1, 28, 28)))
-    model.add(Activation('relu'))
-
+                            padding='same', input_shape=(1, 28, 28),
+                            activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
-    model.add(Convolution2D(filters=56, kernel_size=(7,7),
-                            padding='same'))
-    model.add(Activation('relu'))
 
+    model.add(Convolution2D(filters=56, kernel_size=(7,7),
+                            padding='same',
+                            activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
 
     model.add(Flatten())
@@ -34,6 +33,7 @@ def cnn():
 
     model.add(Dense(10))
     model.add(Activation('softmax'))
+    model.summary()
 
     O=Adam(lr=1e-4)
     model.compile(optimizer=O,
@@ -62,7 +62,6 @@ def dnn():
     model.add(Dropout(0.01))
     model.add(Dense(1024, activation='relu'))
     model.add(Dropout(0.01))
-
     model.add(Dense(10, activation='softmax'))
 
     model.summary()
