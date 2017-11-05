996aa35756e86b445f120370957f46fb69abc1e4
q792602257 <792602257@qq.com>
2017-11-05 08:27:06 +0800

    MachineLearningDemos

--- Python/TF/0.py
+++ Python/TF/0.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+# coding:utf8
 import tensorflow as tf
 import numpy as np
 
 x_d=np.random.rand(1000).astype(np.float32)
-y_d=x_d*2 + 0.5
+y_d=np.random.rand(1000).astype(np.float32)
+z_d=4*x_d*y_d + 0.5
 
 ###
 W=tf.Variable(tf.random_uniform([1],-1.0,1.0))
 B=tf.Variable(tf.zeros([1]))
-y=W*x_d+B
-L=tf.reduce_mean(tf.square(y-y_d))
+z=W*x_d*y_d+B
+L=tf.reduce_mean(tf.square(z-z_d))
 O=tf.train.GradientDescentOptimizer(0.5)
 T=O.minimize(L)
 init = tf.global_variables_initializer()
