c08a7d0a9079f00ed99205624202a20cdd341e64
Jerry Yan <792602257@qq.com>
2018-03-31 21:11:29 +0800

    SC:Use another way to Determine the end of socket and fix some bugs

--- Python/SC/server.py
+++ Python/SC/server.py
@@ -37,69 +37,71 @@ class MySC:
 		elif type(data) == dict or type(data) == list or type(data) == tuple:
 			data_type = "json"
 			data=json.dumps(data)
-		print("[Send][%4s]%s"%(data_type,data))
+		elif type(data) == bytes:
+			data_type = "Raw"
+		print("[Send][%4s]%d Byte"%(data_type,len(data)))
 		data=self.build_data(data_type,data)
 		con.send(data)
 
 	@staticmethod
 	def build_data(data_type,data):
-		retu=data_type+"\r\ns-p-l-i-t\r\n"+data+"\r\nE-N-D"
-		retu=retu.encode()
+		if data_type=="Raw" and type(data)==bytes:
+			retu = b"Raw\r\ns-p-l-i-t\r\n"+data+b"\r\nE-N-D"
+		else:
+			retu=data_type+"\r\ns-p-l-i-t\r\n"+data+"\r\nE-N-D"
+			retu=retu.encode()
 		return retu
 
-	def recv(self,con,wait=False):
+	def recv(self,con):
 		data=b''
 		while True:
-			if wait:
-				while True:
-					try:
-						tmp = con.recv(1024)
-					except ConnectionResetError:
-						return "DISCONNECT"
-					if tmp!=b'':
-						wait=False
-						data+=tmp
-						break
-					else:
-						continue
+			try:
+				tmp = con.recv(1024)
+			except ConnectionResetError:
+				return "D-I-S-C-O-O-N-E-C-T"
+			data+=tmp
+			if tmp.endswith(b"\r\nE-N-D"):
+				break
 			else:
-				tmp = con.recv(1024,)
-				if tmp!=b'':
-					data+=tmp
-				else:
-					break
-			break
+				print("[Recving]%12d Byte"%len(data),'\b'*32+'\r')
+				continue
 		data_type,data=self.divide_data(data)
 		if not data_type:
 			print("Failed On Proceed Recived DATA")
 		else:
-			print("[Recv][%4s]%s"%(data_type,data))
-		if (data_type=="Clnt" or data_type=="SERV") and data=="OK":
-			return "DISCONNECT"
+			print("[Recv][%4s]%d Byte"%(data_type,len(data)))
+		if (data_type=="Clnt" or data_type=="SERV") and data==b"OK":
+			return "D-I-S-C-O-O-N-E-C-T"
 		return data
 
 	@staticmethod
 	def divide_data(data):
-		data=bytes.decode(data)
-		data = data.split("\r\nE-N-D", 1)[0]
-		data_array = data.split("\r\ns-p-l-i-t\r\n", 1)
+		# data=bytes.decode(data)
+		data = data.split(b"\r\nE-N-D", 1)[0]
+		data_array = data.split(b"\r\ns-p-l-i-t\r\n", 1)
+		try:
+			data_type=bytes.decode(data_array[0])
+		except:
+			return False,False			
 		if len(data_array)!=2:
 			print(data_array)
 			return False,False
 		else:
-			if data_array[0]=='bool':
-				if data_array[1]=="T":
+			if data_array[0]==b'bool':
+				data_type=bytes.decode(data_array[0])
+				if data_array[1]==b"T":
 					retu=True
 				else:
 					retu=False
-			elif data_array[0]=='json':
+			elif data_array[0]==b'json':
+				data_type=bytes.decode(data_array[0])
 				try:
-					retu = json.loads(data_array[1])
+					retu = json.loads(bytes.decode(data_array[1]))
 				except:
 					retu = None
 			else:
 				retu = data_array[1]
-			return data_array[0],retu
+			return data_type,retu
 
 	def handle(self,data):
 		print("[Hndl]%s"%data)
@@ -118,9 +120,9 @@ class MySC:
 		if self.SERVER_PART:
 			while True:
 				print("<---%15s:%-5d----"%(addr[0],addr[1]))				
-				data=self.recv(con,True)
+				data=self.recv(con)
 				# server_ok_response(con)
-				if data=="DISCONNECT":
+				if data=="D-I-S-C-O-O-N-E-C-T":
 					print("Disconnect")
 					break
 				else:
@@ -129,14 +131,15 @@ class MySC:
 		else:
 			while True:
 				sleep(1)
-				print("----%15s:%-5d--->" % (self.ADDR, self.PORT))				
-				self.send(con,False)
-				data=self.recv(con,True)
-				if data=="DISCONNECT":
+				print("----%15s:%-5d--->" % (self.ADDR, self.PORT))	
+				with open("cnn.h5","rb") as f:
+					self.send(con,b"".join(f.readlines()))
+				data=self.recv(con)
+				if data=="D-I-S-C-O-O-N-E-C-T":
 					print("Disconnect")
 					break
 				else:
-					continue
+					break
 			self.client_ok_response(con)
 
 MySC()
