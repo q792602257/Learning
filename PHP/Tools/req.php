451cc33292852fd48cec6c0d913b4f2794689720
Jerry Yan <792602257@qq.com>
2018-05-10 15:23:02 +0800

    PHP:Gethtml :: Improved Improve the Response Headers and can build GET Methods query

--- PHP/Tools/req.php
+++ PHP/Tools/req.php
@@ -35,10 +35,24 @@ function getHTML($url,$method="GET",$headers=Null,$data=Null,$cookie=Null){
 		$req.= "Content-type: application/x-www-form-urlencoded\r\n";
 		$req.= "Content-length: ".strlen($post)."\r\n";
 	}
+	if($method=="GET"){
+		if(is_array($data)){
+			$url.="?".http_build_query($data);
+		}
+	}
 	$option=array($urlarr["scheme"]=>array("url"=>$url,"method"=>$method,"header"=>$req,"content"=>$post));
 	$context=stream_context_create($option);
-	$stream = fopen($url,"r",false,$context);
-	$Rheader=stream_get_meta_data($stream)["wrapper_data"];
+	$stream = fopen($url,"rb",false,$context);
+	$_Rheader=stream_get_meta_data($stream)["wrapper_data"];
+	$Rheader=[];
+	foreach($_Rheader as $_L){
+		$_eH = explode(":",$_L,2);
+		if (count($_eH)<2){
+			continue;
+		}else{
+			$Rheader[$_eH[0]]=$_eH[1];
+		}
+	}
 	$html=stream_get_contents($stream);
 	return array("html"=>$html,"header"=>$Rheader);
 }
@@ -46,13 +60,14 @@ if(isset($_GET["url"])){
 	$url=$_GET["url"];
 }
 else{
-	$url = "https://www.himei.top/";
+	$url = "http://scrmtest.changan.com.cn/scrm-app-web/auth/pic/send";
 }
-$ret=getHTML($url);
+$ret=getHTML($url,"GET",Null,["picKey"=>2]);
 $header=$ret["header"];
 $html=$ret["html"];
 // foreach(explode("\n",$header) as $line){
 // 	header($line);
 // }
 print_r($header);
+print_r($html);
 ?>
