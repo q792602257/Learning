844e9ff4ab1994bc6951c19f7e5d16d30d4942ac
Jerry Yan <792602257@qq.com>
2018-05-15 18:10:41 +0800

    And Make Some Branches

--- Python/Tools/delete small Inages.py
+++
@@ -1,9 +0,0 @@
-import os,sys,Image,glob
-
-  
-s=glob.glob(os.path.join(os.path.abspath(''),'wallpaper*','*'))
-for f in s:
-    im=Image.open(f)
-    if (im.size[0]<1000 or im.size[1]<700):
-        os.remove(f)
-        print f+'\t'+str(im.size)
